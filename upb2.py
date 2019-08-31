dafa99f763157d637c2a0acf3a97e9adea6e8bd6
Raunak <sharmaraunak338@gmail.com>
2019-08-31 19:43:07 +0530

    data structures in python

---
+++ upb2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 10:51:27 2019
+
+@author: raunak
+"""
+
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    # Your code goes here!
+    for i in range(5):
+        print("I am super excited for this course! ")
+    
+    return "return"
+
+print (show_excitement())
