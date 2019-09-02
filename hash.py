72f26a40e76d739f2d24429583bae101eff16e4e
Raunak <sharmaraunak338@gmail.com>
2019-09-03 00:23:18 +0530

    hash

--- hash.py
+++ hash.py
@@ -9,36 +9,44 @@ class HashTable(object):
     def store(self, string):
         """Input a string that's stored in 
         the table."""
-        pass
+        hash_value = self.calculate_hash_value(string)
+        self.table[hash_value] = string
+
+    def calculate_hash_value(self, string):
+        """Helper function to calulate a
+        hash value from a string."""
+        hash_value = ord(string[0])*100+ord(string[1])
+        
+        return hash_value        
 
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
+        hash_value = self.calculate_hash_value(string)
+        if(self.table[hash_value]!= None):
+            return hash_value
         return -1
 
-    def calculate_hash_value(self, string):
-        """Helper function to calulate a
-        hash value from a string."""
-        return -1
+   
     
 # Setup
 hash_table = HashTable()
 
 # Test calculate_hash_value
 # Should be 8568
-print hash_table.calculate_hash_value('UDACITY')
+print (hash_table.calculate_hash_value('UDACITY'))
 
 # Test lookup edge case
 # Should be -1
-print hash_table.lookup('UDACITY')
+print (hash_table.lookup('UDACITY'))
 
 # Test store
 hash_table.store('UDACITY')
 # Should be 8568
-print hash_table.lookup('UDACITY')
+print (hash_table.lookup('UDACITY'))
 
 # Test store edge case
 hash_table.store('UDACIOUS')
 # Should be 8568
-print hash_table.lookup('UDACIOUS')
+print (hash_table.lookup('UDACIOUS'))
